b2374c1ca984b117f7d89d4d63b0cf0db9b945fb
saiful <sohags3333@gmail.com>
2023-05-27 11:00:23 +0600

    Nothing to commit

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'afa98f1bdeaedcbb94cd');
+<?php return array('dependencies' => array('lodash', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'e05449720d22d10bcd65');
